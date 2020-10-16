ef48737bc2f518e7e1865b2ff1b490d866236851
Kishor Naik <kishor.naik011.net@outlook.com>
2020-10-16 15:33:00 +0530

    Code Commit

---
+++ Sol_Demo/Sol_Demo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Sol_Demo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var path = @"D:/Test.txt";
+
+            using var fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            using var sr = new StreamReader(fs, Encoding.UTF8);
+
+            string line = String.Empty;
+
+            while ((line = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            };
+
+            // Dispose Automatically
+        }
+    }
+}
